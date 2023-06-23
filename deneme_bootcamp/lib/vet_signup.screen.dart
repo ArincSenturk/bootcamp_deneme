56ccd1122dd0793672e6f1ced7eade4b03766f26
ArincSenturk <arincsenturk@gmail.com>
2023-06-23 13:35:38 +0300

    update vet_signup.screen.dart

--- deneme_bootcamp/lib/vet_signup.screen.dart
+++ deneme_bootcamp/lib/vet_signup.screen.dart
@@ -14,11 +14,17 @@ class VetRegisterPage extends StatefulWidget {
 
 class _VetRegisterPageState extends State<VetRegisterPage> {
   late File _selectedImage;
+   late File _secondSelectedImage;
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
   TextEditingController _confirmPasswordController = TextEditingController();
+  TextEditingController _namesurnameController = TextEditingController();
+  TextEditingController _universityController = TextEditingController();
+  TextEditingController _speciesController = TextEditingController();
+  bool _checkValue1 = false;
+  bool _checkValue2 = false;
 
     Future<void> _pickImage() async {
     final pickedImage = await ImagePicker().getImage(source: ImageSource.gallery);
@@ -28,6 +34,15 @@ class _VetRegisterPageState extends State<VetRegisterPage> {
       });
     }
   }
+  Future<void> _pickSecondImage() async {
+    final pickedImage = await ImagePicker().getImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {
+        _secondSelectedImage = File(pickedImage.path);
+      });
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -60,6 +75,35 @@ class _VetRegisterPageState extends State<VetRegisterPage> {
                           controller: _emailController,
                           decoration: InputDecoration(
                             labelText: 'E-mail',
+                            border: InputBorder.none
+                            ),
+                          
+                        ),
+                      ),
+                    ),SizedBox(height: 10,),
+                    DecoratedBox(
+                      decoration: BoxDecoration(color: Color.fromARGB(218, 177, 78, 195),borderRadius: BorderRadius.circular(12)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left:20.0),
+                        child: TextFormField(
+                          controller: _namesurnameController,
+                          decoration: InputDecoration(
+                            labelText: 'Ad Soyad',
+                            border: InputBorder.none
+                            ),
+                          
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10,),
+                    DecoratedBox(
+                      decoration: BoxDecoration(color: Color.fromARGB(218, 177, 78, 195),borderRadius: BorderRadius.circular(12)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left:20.0),
+                        child: TextFormField(
+                          controller: _universityController,
+                          decoration: InputDecoration(
+                            labelText: 'Üniversiteniz',
                             border: InputBorder.none
                             ),
                           
@@ -98,10 +142,58 @@ class _VetRegisterPageState extends State<VetRegisterPage> {
                           
                         ),
                       ),
-                    ),SizedBox(height: 20.0),
+                    ),
+                    SizedBox(height: 10,),
+                    DecoratedBox(
+                      decoration: BoxDecoration(color: Color.fromARGB(218, 177, 78, 195),borderRadius: BorderRadius.circular(12)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left:20.0),
+                        child: TextFormField(
+                          controller: _speciesController,
+                          decoration: InputDecoration(
+                            labelText: 'Baktığınız Türler? örnek: Kedi,Balık,Sincap...',
+                            border: InputBorder.none
+                            ),
+                          
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
                     ElevatedButton(
               onPressed: _pickImage,
-              child: Text('Resim Seç'),),
+              child: Text('E-devlet Mezuniyet Belgesi Yükle'),),
+              SizedBox(height: 20.0),
+                    ElevatedButton(
+                      onPressed: _pickSecondImage,
+                      child: Text('Profil Resmi Yükle'),
+                    ), SizedBox(height: 10,),
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: _checkValue1,
+                          onChanged: (value) {
+                            setState(() {
+                              _checkValue1 = value!;
+                            });
+                          },
+                        ),
+                        Text('Tedavi İçin Evlere Giderim'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: _checkValue2,
+                          onChanged: (value) {
+                            setState(() {
+                              _checkValue2 = value!;
+                            });
+                          },
+                        ),
+                        Text('Acil Hasta Bakarım'),
+                      ],
+                    ),
+              
                     SizedBox(height: 20.0),
                     // Kayıt ol butonu
                     SizedBox(width: 345,height: 50,
@@ -145,10 +237,26 @@ class _VetRegisterPageState extends State<VetRegisterPage> {
     await userDocRef.set({
       'email': userCredential.user!.email,
       'isVet': 'false', // Kullanıcının adını veya diğer bilgileri buraya ekleyebilirsiniz
+       'name': _namesurnameController.text,
+       'university': _universityController.text,
+       'species': _speciesController.text,
     });
     // Resmi Firebase Storage'a yükle
       final storageRef = FirebaseStorage.instance.ref().child('users/$uid/profile_image.jpg');
       await storageRef.putFile(_selectedImage);
+
+      // Profil resmi Firebase Storage'a yükle
+      final secondStorageRef = FirebaseStorage.instance
+          .ref()
+          .child('profile_picture/$uid/profile_picture.jpg');
+      await secondStorageRef.putFile(_secondSelectedImage);
+
+      // Checkbox değerlerini Firestore'a yazdırma
+      await userDocRef.update({
+        'option1': _checkValue1,
+        'option2': _checkValue2,
+      });
+
 
       print('Kullanıcı kaydedildi, Firestore dökümanı oluşturuldu ve resim Firebase Storage\'a yüklendi.');
 
